b7edb13ca8d079818aafc44173430bc10bbbe1d7
SANJEEVA RAYUDU SULAM <152423974+sulamsanjeevarayudu@users.noreply.github.com>
2026-03-22 22:04:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,414 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Mana Telugu Pellillu</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <section id="header">
+            <a href="#"><img src="img/logo1.png.png" class="logo" alt=""></a>
+
+            <div>
+                <ul id="navbar">
+                    <li><a class="active" href="home.html">Home</a></li>
+                 
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li id="lg-bag"><a href="cart.html"><i class='bx bx-shopping-bag' ></i></a></li>
+                    <a href="#" id="close"><i class='bx bx-x'></i></a>
+                </ul>
+            </div>
+            <div id="mobile">
+                <a href="cart.html"><i class='bx bx-shopping-bag' ></i></a>
+                <i id="bar" class='bx bx-menu'></i>
+            </div>
+        </section>
+
+        <section id="main">
+            <h4></h4>
+            <h2>Super value deals</h2>
+            <h1>For all events</h1>
+            <p>save more with coupons & up to 20% off!</p>
+            <button class="btn">Book Now</button>
+        </section> 
+
+        <section id="feature" class="section-p1">
+            <div class="fe-box">
+                <img src="img/features/freeship.jpg" alt="">
+                <h6>Free Shipping</h6>
+            </div>
+           
+            <div class="fe-box">
+                <img src="img/features/save.jpg" alt="">
+                <h6>Save Money</h6>
+            </div>
+            <div class="fe-box">
+                <img src="img/features/happy.jpg" alt="">
+                <h6>Promotions</h6>
+            </div>
+            <div class="fe-box">
+                <img src="img/features/dis.jpg" alt="">
+                <h6>Special Discounts </h6>
+            </div>
+            <div class="fe-box">
+                <img src="img/features/f24.jpg" alt="">
+                <h6>F24/7 Support</h6>
+            </div>
+        </section>
+
+        <section id="product1" class="section-p1">
+            <h2>Events</h2>
+            <p></p>
+            <div class="pro-container">
+                <div class="pro">
+                    <img class="shirt" src="img/products/hal9.jpg" alt="">
+                    <div class="des">
+                        <span>Haldi</span>
+                        <h5>Haldi Event Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹24,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/eng3.jpg" alt="">
+                    <div class="des">
+                        <span>Engagement</span>
+                        <h5>Engagement Stage Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹89,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/sang1.jpg" alt="">
+                    <div class="des">
+                        <span>Sangeet</span>
+                        <h5>Sangeet Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹54,499</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/cat1.jpg" alt="">
+                    <div class="des">
+                        <span>Dinning</span>
+                        <h5>Dinning Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹65,499</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/pel1.jpg" alt="">
+                    <div class="des">
+                        <span>South Wedding</span>
+                        <h5>South Wedding Mandapam Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹1,25,99</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/din.webp" alt="">
+                    <div class="des">
+                        <span>Catering</span>
+                        <h5>Catering Services</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹99,499</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/pel2.jpg" alt="">
+                    <div class="des">
+                        <span>North Wedding</span>
+                        <h5>North Wedding Mandapam Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹1,22,499</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/meh1.jpg" alt="">
+                    <div class="des">
+                        <span>Mehndi</span>
+                        <h5>Mehndi Event Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹55,499</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+            </div>
+        </section>
+
+        <section id="product1" class="section-p1">
+          
+            <div class="pro-container">
+                <div class="pro">
+                    <img class="shirt" src="img/products/hal3.jpg" alt="">
+                    <div class="des">
+                        <span>Haldi</span>
+                        <h5>Haldi Event Collection</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹29,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/hal2.jpg" alt="">
+                    <div class="des">
+                        <span>Haldi</span>
+                        <h5>Haldi Event Collection
+                        </h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹25,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/eng2.jpg" alt="">
+                    <div class="des">
+                        <span>Engagement</span>
+                        <h5>Engagement Stage Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹23,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/eng5.jpg" alt="">
+                    <div class="des">
+                        <span>Engagement</span>
+                        <h5>Engagement Stage Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹39,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/pel3.jpg" alt="">
+                    <div class="des">
+                        <span>Pelli</span>
+                        <h5>Pelli mandapam Event</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹2999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/pel4.jpg" alt="">
+                    <div class="des">
+                        <span>Pelli</span>
+                        <h5>Pelli Mandapam</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹2999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/wedd1.jpg" alt="">
+                    <div class="des">
+                        <span>Wedding</span>
+                        <h5>Wedding Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹59,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+                <div class="pro">
+                    <img class="shirt" src="img/products/wedd2.jpg" alt="">
+                    <div class="des">
+                        <span>wedding</span>
+                        <h5>Wedding Decoration</h5>
+                        <div class="star">
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                            <i class='bx bxs-star' ></i>
+                        </div>
+                        <h4>₹59,999</h4>
+                    </div>
+                    <a href="#"><i class='bx bx-cart cart' ></i></a>
+                </div>
+            </div>
+        </section>
+
+        <section id="sm-banner" class="section-p1">
+
+            
+        </section>
+
+        <section id="banner3">
+            <div class="banner-box">
+                <h2>SEASONAL SALE</h2>
+                <h3>Wedding Event Collection -40% OFF</h3>
+            </div>
+            
+        </section>
+
+        <section id="newsletter" class="section-p1 section-m1">
+            <div class="newstext">
+                <h4>Sign Up For Booking</h4>
+                <p>Get E-mail updates about our latest events and <span>special offers.</span></p>
+            </div>
+            <div class="form">
+                <input type="text" placeholder="Your email address">
+                <button class="normal">Sign Up</button>
+            </div>
+        </section>
+
+        <footer class="section-p1">
+            <div class="col">
+                <img class="logo" src="./img/logo1.png.png" alt="">
+                <h4>Contact</h4>
+                <p><strong>Address:</strong> DEVARAKAGGALAHALLI, HAROHALLI, Kanakapura Rd, Kanakapura, Karnataka 562112
+                    </p>
+                <p><strong>Phone:</strong> +91 123456789/ (+91) 0123456789</p>
+                <p><strong>Hours:</strong> 10:00 - 18:00, Mon - Sat</p>
+                <div class="follow">
+                    <h4>Follow us</h4>
+                    <div class="icon">
+                        <i class='bx bxl-facebook'></i>
+                        <i class='bx bxl-twitter' ></i>
+                        <i class='bx bxl-instagram' ></i>
+                        <i class='bx bxl-pinterest-alt' ></i>
+                        <i class='bx bxl-youtube' ></i>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <h4>About</h4>
+                <a href="#">About us</a>
+                <a href="#">Devlivery Information</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms & Conditions</a>
+                <a href="#">Contact Us</a>
+            </div>
+
+            <div class="col">
+                <h4>My Account</h4>
+                <a href="#">Sign In</a>
+                <a href="#">View Cart</a>
+                <a href="#">My Wishlist</a>
+                <a href="#">Track My Order</a>
+                <a href="#">Help</a>
+            </div>
+
+            <div class="col install">
+                <h4>Install App</h4>
+                <p>From App Store or Google Play</p>
+                <div class="row">
+                    <img src="./img/pay/app.jpg" alt="">
+                    <img src="./img/pay/play.jpg" alt="">
+                </div>
+                <p>Secured Payment Gateways</p>
+                <img src="./img/pay/pay.png" alt="">
+            </div>
+
+         
+        </footer>
+
+        <script src="script.js"></script>
+    </body>
+</html>
